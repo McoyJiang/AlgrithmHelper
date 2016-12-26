ede11bb8736f849fa6ba3c6a6b45d5f8263e5a43
DannyMcoy <1240784879@qq.com>
2016-12-26 17:38:46 +0800

    添加字符串旋转和字符串全排列

--- SearchHelper.java
+++ SearchHelper.java
@@ -137,4 +137,85 @@ public class SearchHelper {
     }
     /**************************字符串无序匹配 END*********************************/
 
+
+    /**************************字符串翻转 BEGIN******************************************/
+    /**
+     * 题目
+     * 给定一个字符，要求将字符串前面的若干个字符串移动到字符串的尾部
+     * 例如字符串"abcdef"的前3个字符移动到尾部后为"defabc"
+     */
+    /**
+     * 将字符串string中从0下标开始到index下标的子串移动到尾部
+     * @param string
+     * @param index
+     * @return
+     */
+    public static String leftToRightString(char[] string, int index) {
+        int length = string.length;
+
+        reverseString(string, 0, index - 1);
+
+        reverseString(string, index, length - 1);
+
+        reverseString(string, 0, length - 1);
+
+        return new String(string);
+    }
+
+    /**
+     * 将s字符串完全颠倒  abc -> cba
+     * @param string
+     * @param from
+     * @param to
+     */
+    private static void reverseString(char[] string, int from, int to){
+        while (from < to) {
+            char charAt = string[from];
+
+            string[from++] = string[to];
+
+            string[to--] = charAt;
+        }
+    }
+    /**************************字符串翻转 END******************************************/
+
+    /**************************字符串全排列 BEGIN*********************************/
+    /**
+     * 题目：输入一个字符串,打印出该字符的所有排列
+     * 例如：输入"abc", 怎输出 abc acb bac bca cab cba六种
+     * 思路：采用递归的方式，依次对后续的子串进行全排列，当递归到只有一个字母时，则打印出当前的字符串
+     * 1 将a固定在第一位，求后面bc的排列，得到：abc和acb
+     * 2 将b固定在第一位，求后面bc的排列，得到：bac和bca
+     * 3 将c固定在第一位，求后面bc的排列，得到：cab和cba
+     * @param arr   需要进行全排列的字符数组，需要将String转化为char数组
+     * @param start 需要全排列的子串下边，一般从0开始
+     * @param end   需要全排列的结束为止，一般为length - 1
+     */
+    public static void permutation(char[] arr, int start, int end) {
+        if (start == end) { // 当只要求对数组中一个字母进行全排列时，只要就按该数组输出即可
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i <= end; i++) {
+                System.out.print(arr[i]);
+                stringBuilder.append(arr[i]);
+            }
+            stringBuilder.append("\n");
+            Log.e("TAG", stringBuilder.toString());
+        } else { // 对多字符进行递归替换调用
+            for (int i = start; i <= end; i++) {
+                //对首字母进行替换操作
+                char temp = arr[start];
+                arr[start] = arr[i];
+                arr[i] = temp;
+
+                //递归调用start + 1开始的子串
+                permutation(arr, start + 1, end);
+
+                //递归调用完毕之后，需要还原， 比如从bac从新还原回abc
+                temp = arr[i];
+                arr[i] = arr[start];
+                arr[start] = temp;
+            }
+        }
+    }
+    /**************************字符串全排列 END*********************************/
 }
